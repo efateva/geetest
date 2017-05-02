7e4772c56e97a8459766d4be7021e7475d4ef610
darbra <darbra@126.com>
2017-05-02 14:09:23 +0800

    darbra
    darbra

--- geetest_selenium.py
+++ geetest_selenium.py
@@ -1,198 +1,164 @@
 #-*-coding:utf-8-*-
-
-from selenium import webdriver
-from PIL import Image
-import time
-import random
-import re
 import requests
+import re
+import StringIO
+from PIL import Image
+import random
 import math
+import time
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-"""
-dcap = dict(DesiredCapabilities.PHANTOMJS)
-dcap["phantomjs.page.settings.userAgent"] = (
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36"
-        )
-
-br = webdriver.PhantomJS(desired_capabilities=dcap)
-"""
-br = webdriver.Chrome()
-times = 1
-while times > 0:
-    br.get("http://www.jianshu.com/sign_in")
-    wait = WebDriverWait(br, 40, 1.0)
-    element = wait.until(EC.presence_of_element_located((By.ID, "session_email_or_mobile_number")))
-    element.send_keys("")
-    time.sleep(1.2)
-    element = wait.until(EC.presence_of_element_located((By.ID, "session_password")))
-    element.send_keys("")
-    time.sleep(1.2)
+from selenium import webdriver
 
 
-    
-
-    hd = {
-'Accept':'image/webp,*/*;q=0.8',
-'Accept-Encoding':'gzip,deflate,sdch',
-'Accept-Language':'zh-CN,zh;q=0.8',
-'Connection':'keep-alive',
-'Host':'static.geetest.com',
-'Origin':'http://www.jianshu.com',
-'Referer':'http://www.jianshu.com/sign_in',
-'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1979.0 Safari/537.36',       }
+class crack_picture(object):
+    def __init__(self, img_url1, img_url2):
+        self.img1, self.img2 = self.picture_get(img_url1, img_url2)
 
 
-    wait = WebDriverWait(br, 40, 1.0)
-    element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "gt_cut_fullbg_slice")))
-    print element.get_attribute('style')
-    try:
-        tmp1 = re.findall('url\("(.*?)"\)', element.get_attribute('style'))[0].replace("webp", "jpg")
-    except:
-        tmp1 = re.findall('url\((.*?)\)', element.get_attribute('style'))[0].replace("webp", "jpg")
-    print tmp1
+    def picture_get(self, img_url1, img_url2):
+        hd = {"Host": "static.geetest.com",
+              "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
+        img1 = StringIO.StringIO(self.repeat(img_url1, hd).content)
+        img2 = StringIO.StringIO(self.repeat(img_url2, hd).content)
+        return img1, img2
 
-    element = br.find_element_by_class_name("gt_cut_bg_slice")
-    try:
-        tmp2 = re.findall('url\("(.*?)"\)', element.get_attribute('style'))[0].replace("webp", "jpg")
-    except:
-        tmp2 = re.findall('url\((.*?)\)', element.get_attribute('style'))[0].replace("webp", "jpg")
-    print tmp2
-    cookies = {}
-    for i in br.get_cookies():
-        cookies[i['name']] = i['value']
-    print cookies
-    tf = file("fullbg.jpg", "wb")
-    tf.write(requests.get(tmp1).content)
-    tf.close()
 
-    tf = file("bg.jpg", "wb")
-    tf.write(requests.get(tmp2).content)
-    tf.close()
+    def repeat(self, url, hd):
+        times = 10
+        while times > 0:
+            try:
+                ans = requests.get(url, headers=hd)
+                return ans
+            except:
+                times -= 1   
 
-    def image_process():
+
+    def pictures_recover(self):
+        xpos = self.judge(self.picture_recover(self.img1, 'img1.jpg'), self.picture_recover(self.img2, 'img2.jpg')) - 6
+        return self.darbra_track(xpos)
+
+
+    def picture_recover(self, img, name):
         a =[39, 38, 48, 49, 41, 40, 46, 47, 35, 34, 50, 51, 33, 32, 28, 29, 27, 26, 36, 37, 31, 30, 44, 45, 43, 42, 12, 13, 23, 22, 14, 15, 21, 20, 8, 9, 25, 24, 6, 7, 3, 2, 0, 1, 11, 10, 4, 5, 19, 18, 16, 17]
-
-        im = Image.open("bg.jpg")
-        im_new = Image.new("RGB",(260, 116))
-        width, height = im.size
-
+        im = Image.open(img)
+        im_new = Image.new("RGB", (260, 116))
         for row in range(2):
             for column in range(26):
-                
                 right = a[row*26+column] % 26 * 12 + 1
                 down = 58 if a[row*26+column] > 25 else 0
                 for w in range(10):
                     for h in range(58):
                         ht = 58 * row + h
                         wd = 10 * column + w
-                        #print ht, wd
                         im_new.putpixel((wd, ht), im.getpixel((w + right, h + down)))
+        im_new.save(name)
+        return im_new
 
 
-        im_new.save("bg_after.jpg")
-
-        im = Image.open("fullbg.jpg")
-        im_new = Image.new("RGB",(260, 116))
-        width, height = im.size
-
-        for row in range(2):
-            for column in range(26):
-                
-                right = a[row*26+column] % 26 * 12 + 1
-                down = 58 if a[row*26+column] > 25 else 0
-                for w in range(10):
-                    for h in range(58):
-                        ht = 58 * row + h
-                        wd = 10 * column + w
-                        #print ht, wd
-                        im_new.putpixel((wd, ht), im.getpixel((w + right, h + down)))
-
-
-        im_new.save("fullbg_after.jpg")
-
-
-        img1 = Image.open("bg_after.jpg")
-
-        img2 = Image.open("fullbg_after.jpg")
-
-        def diff(img1, img2, wd, ht):
-            rgb1 = img1.getpixel((wd, ht))
-            rgb2 = img2.getpixel((wd, ht))
-            tmp = 0
-            for i in range(3):
-                if abs(rgb1[i]-rgb2[i]) > 50:
-                    tmp += 1
-            if tmp == 3:
-                return True
-            return False
-                
-        def col(img1, img2, cl):
-            for i in range(img2.size[1]):
-                if diff(img1, img2, cl, i):
-                    return True
-            return False
-
-        def judge(img1, img2):
-            for i in range(img2.size[0]):
-                if col(img1, img2, i):
-                    return i
-            return -1
-
-        xpos = judge(img1, img2) - 6
-
-        print xpos
-        return xpos 
-
-    xpos = image_process()
-
-
-
-    def darbra_track(distance):
-
-        
+    def darbra_track(self, distance):
         last_move = random.randint(3,5) * random.choice([1, 1])
         distance += last_move
         maxdst = distance
         ans = []
         while distance > 0:
-
-        #轨迹代码已删除
-
+            #轨迹代码已删除
         return ans
+
+
+    def diff(self, img1, img2, wd, ht):
+        rgb1 = img1.getpixel((wd, ht))
+        rgb2 = img2.getpixel((wd, ht))
+        tmp = reduce(lambda x,y: x+y, map(lambda x: abs(x[0]-x[1]), zip(rgb1, rgb2)))
+        return True if tmp >= 200 else False
+
+            
+    def col(self, img1, img2, cl):
+        for i in range(img2.size[1]):
+            if self.diff(img1, img2, cl, i):
+                return True
+        return False
+
+
+    def judge(self, img1, img2):
+        for i in range(img2.size[0]):
+            if self.col(img1, img2, i):
+                return i
+        return -1
+
+
+class jianshu(object):
+    def __init__(self, br_name="phantomjs"):
+        self.br = self.get_webdriver(br_name)
+        self.wait = WebDriverWait(self.br, 40, 1.0)
+
+
+    def input_params(self, name, password):
+        self.br.get("http://www.jianshu.com/sign_in")
+        element = self.wait_for(By.ID, "session_email_or_mobile_number")
+        element.send_keys(name)
+        time.sleep(1.1)
+        element = self.wait_for(By.ID, "session_password")
+        element.send_keys(password)
+        time.sleep(1.1)
+        return (self.find_img_url(self.wait_for(By.CLASS_NAME, "gt_cut_fullbg_slice")),
+               self.find_img_url(self.wait_for(By.CLASS_NAME, "gt_cut_bg_slice")))
         
-
-
-    array_trail = darbra_track(xpos)
-
-
-    element = br.find_element_by_class_name("gt_slider_knob")
-    ActionChains(br).click_and_hold(on_element=element).perform()
-    for x, y, t in array_trail:
-        print x, y ,t 
-        ActionChains(br).move_to_element_with_offset(
-                    to_element=element, 
-                    xoffset=x+22,
-                    yoffset=y+22).perform()
-        ActionChains(br).click_and_hold().perform()
-        time.sleep(t)
-    time.sleep(0.24)
-    ActionChains(br).release(on_element=element).perform()
-    time.sleep(2)
-    wait = WebDriverWait(br, 40, 1.0)
-    element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "gt_info_text")))
-    print element.text
-
-    wait = WebDriverWait(br, 40, 1.0)
-    element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "sign-in-button")))
-    element.submit()
-    time.sleep(10)
-
-
     
-    times -= 1
+    def wait_for(self, by1, by2):
+        return self.wait.until(EC.presence_of_element_located((by1, by2)))
 
-br.quit()
+
+    def find_img_url(self, element):
+        try:
+            return re.findall('url\("(.*?)"\)', element.get_attribute('style'))[0].replace("webp", "jpg")
+        except:
+            return re.findall('url\((.*?)\)', element.get_attribute('style'))[0].replace("webp", "jpg")
+
+
+    def emulate_track(self, tracks):
+        element = self.br.find_element_by_class_name("gt_slider_knob")
+        ActionChains(self.br).click_and_hold(on_element=element).perform()
+        for x, y, t in tracks:
+            print x, y ,t 
+            ActionChains(self.br).move_to_element_with_offset(
+                        to_element=element, 
+                        xoffset=x+22,
+                        yoffset=y+22).perform()
+            ActionChains(self.br).click_and_hold().perform()
+            time.sleep(t)
+        time.sleep(0.24)
+        ActionChains(self.br).release(on_element=element).perform()
+        time.sleep(2)
+        element = self.wait_for(By.CLASS_NAME, "gt_info_text")
+        print element.text.encode("utf-8")
+
+
+    def hack_geetest(self):
+        img_url1, img_url2 = self.input_params("a", "b")
+        tracks = crack_picture(img_url1, img_url2).pictures_recover()
+        self.emulate_track(tracks)
+        self.quit_webdriver()
+
+
+    def quit_webdriver(self):
+        self.br.quit()
+
+
+    def get_webdriver(self, name):
+        if name.lower() == "phantomjs":
+            dcap = dict(DesiredCapabilities.PHANTOMJS)
+            dcap["phantomjs.page.settings.userAgent"] = (
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36")
+            return webdriver.PhantomJS(desired_capabilities=dcap)
+
+        elif name.lower() == "chrome":
+            return webdriver.Chrome()
+
+
+if __name__ == "__main__":
+    #print crack_picture("http://static.geetest.com/pictures/gt/fc064fc73/fc064fc73.jpg", "http://static.geetest.com/pictures/gt/fc064fc73/bg/7ca363b09.jpg").pictures_recover()
+    jianshu().hack_geetest()
